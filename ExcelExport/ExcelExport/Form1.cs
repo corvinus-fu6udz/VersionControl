13a2b91eec0b76bdf1719f48666b5e48a6b991b4
Balázs Árvai <balazs.arvai@stud.uni-corvinus.hu>
2021-10-14 16:02:24 +0200

    6. Feladat

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -16,15 +16,53 @@ namespace ExcelExport
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
-        private void LoadData()
+        public void LoadData()
         {
-            Flats = context.Flats.ToList(); //adattábla másolása a memóriába
+            Flats = context.Flats.ToList(); // Adattábla másolása a memóriába
+        }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application(); //  Excel elindítása és az applikáció objektum betöltése
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value); // Új munkafüzet
+
+                xlSheet = xlWB.ActiveSheet; // Új munkalap
+
+                CreateTable();
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+        public void CreateTable()
+        {
+
         }
     }
 }
