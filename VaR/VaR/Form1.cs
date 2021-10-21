0c5f132aca3f582267efe459a98fee04f7fc1bbf
Balázs Árvai <balazs.arvai@stud.uni-corvinus.hu>
2021-10-21 17:38:56 +0200

    5. Feladat

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -7,18 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VaR.Entities;
 
 namespace VaR
 {
     public partial class Form1 : Form
     {
         List<Tick> ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
             ticks = context.Ticks.ToList();
             dataGridView1.DataSource = ticks;
+            CreatePortfolio();
+        }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
