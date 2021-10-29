45ae2c646b4459a36efc2e3e97daa02b5d6878a4
Balázs Árvai <balazs.arvai@stud.uni-corvinus.hu>
2021-10-29 18:37:51 +0200

    6. Feladat

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using Webszolgaltatas.Entities;
 using Webszolgaltatas.MnbServiceReference;
@@ -23,6 +24,22 @@ namespace Webszolgaltatas
             string xmlString = Consume();
             LoadXml(xmlString);
             dataGridView1.DataSource = Rates;
+            Charting();
+        }
+        private void Charting()
+        {
+            chartRateData.DataSource = Rates;
+            Series series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
         }
         private void LoadXml(string input)
         {
