41874c3a61552535adec23c839d230173f24e401
Balázs Árvai <balazs.arvai@stud.uni-corvinus.hu>
2021-11-26 20:14:28 +0100

    4. Feladat

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,12 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
     class AccountControllerTestFixture
     {
-        [Test]
+        [Test,
+        TestCase("abcd1234", false),
+        TestCase("irf@uni-corvinus", false),
+        TestCase("irf.uni-corvinus.hu", false),
+        TestCase("irf@uni-corvinus.hu", true)]
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
